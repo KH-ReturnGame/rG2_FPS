a7dc142de0e356ad9cf1d9493014fa1a95d9e451
Monakai0 <164456230+Monakai0@users.noreply.github.com>
2025-05-17 23:09:25 +0900

    로비 준비중 텍스트 추가

--- Assets/#1 Scripts/Manager/Lobby.cs
+++ Assets/#1 Scripts/Manager/Lobby.cs
@@ -1,10 +1,50 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro; 
 
 public class Lobby : MonoBehaviour
 {
+    [Header("UI Components")]
+    public CanvasGroup preparing;
+
+    public void Start()
+    {
+        preparing.gameObject.SetActive(false);
+    }
+
     public void Tutorial()
     {
         SceneLoader.LoadScene("Main");
+    }
+    
+    public void Preparing()
+    {
+        StartCoroutine(ShowPreparingText());
+    }
+
+    private IEnumerator ShowPreparingText()
+    {
+        preparing.gameObject.SetActive(true);
+
+        float speed = 8f;
+        for (float a = 0; a < 1f; a += Time.deltaTime * speed)
+        {
+            preparing.alpha = a;
+            yield return null;
+        }
+        preparing.alpha = 1f;
+
+        yield return new WaitForSeconds(0.5f);
+        
+        for (float a = 1f; a > 0f; a -= Time.deltaTime * speed)
+        {
+            preparing.alpha = a;
+            yield return null;
+        }
+        preparing.alpha = 0f;
+
+        preparing.gameObject.SetActive(false);
     }
 }
